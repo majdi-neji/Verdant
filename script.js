4891e8559dd000a5982b384af7109ea1c5a3e93e
Majdi Neji <48561788+majdi-neji@users.noreply.github.com>
2026-08-13 04:36:07 +0000

    clean up

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const navLinks = document.querySelector(".primary-navigation");
 
-hamburger.addEventListener("click", () => {
-  // Toggle the visual active states
-  hamburger.classList.toggle("open");
-  navLinks.classList.toggle("open");
+if (hamburger && navLinks) {
+  hamburger.addEventListener("click", () => {
+    // Toggle the visual active states
+    hamburger.classList.toggle("open");
+    navLinks.classList.toggle("open");
 
-  // Update accessibility state for screen readers
-  const isOpen = hamburger.classList.contains("open");
-  hamburger.setAttribute("aria-expanded", isOpen);
-});
+    // Update accessibility state for screen readers
+    const isOpen = hamburger.classList.contains("open");
+    hamburger.setAttribute("aria-expanded", isOpen);
+  });
+}
